312afd202449da431e50c7f189ca1d9af09be1a9
Deepak Bhaskaran <p3printz@gmail.com>
2026-01-26 22:50:37 -0500

    Enabled relative urls

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ SITENAME = 'Purple Pillz Printz'
 
 # If your site is available via HTTPS
 SITEURL = 'https://p3printz.github.io'
-RELATIVE_URLS = False
+RELATIVE_URLS = True
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
